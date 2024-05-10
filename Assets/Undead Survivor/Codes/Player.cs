5e12e33855d786f68b1f6761cde9ebd057fa308f
vmffpdlwnd <leegunehee@gmail.com>
2024-05-10 14:43:43 +0900

    5월 9일 작업 1차
    캐릭터 좌우반전 추가
    캐릭터 애니메이션 추가
    2번째 캐릭터 애니메이션 추가
    - 골드메탈 밸서라이크 03까지 -

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -7,11 +7,16 @@ public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed;
+
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -23,5 +28,15 @@ public class Player : MonoBehaviour
     private void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed",inputVec.magnitude);
+        
+
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
